49e87b848513d8c4955f03a115470c9699fb5b7a
johnwheeler <john@johnwheeler.org>
2016-08-05 21:36:57 -0700

    initial

---
+++ demo.py
@@ -0,0 +1,19 @@
+import logging
+from flask import Flask
+from flask_ask import Ask, statement, question
+
+
+app = Flask(__name__)
+ask = Ask(app, "/")
+logging.getLogger("flask_ask").setLevel(logging.DEBUG)
+
+
+@ask.launch
+def welcome():
+    return statement("Hello PyLadies. Welcome to today's presentation: Flask-Ask, an Alexa Skills Kit framework for Python")
+
+
+@ask.intent("AddIntent")
+def add(x, y):
+    the_sum = x + y
+    return statement("The sum of {} plus {} is {}".format(x, y, the_sum))
